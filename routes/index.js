1c6003f1e76476aa61edfe1d09c6a2fab100dfb6
farandbeyond <connorjh4@gmail.com>
2016-11-30 23:01:44 -0500

    final updates

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var passport = require('passport');
 // GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Express',
+    title: 'Connor s Projects',
     user: req.user,
     pageNum: 1
   });
--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,31 @@ var router = express.Router();
 
 // GET users listing.
 router.get('/', function(req, res, next) {
-  res.render('users',{
-      title: 'users',
-      user: req.user,
-      pageNum: 5
-  });
+
+    /*
+    // use Project model to run a query
+    Account.find(function(err, users) {
+        if (err) {
+            console.log(err);
+            res.render('error');
+        }
+        else {
+            // load the project view
+            res.render('users',{
+                title: 'users',
+                user: req.user,
+                userlist: users,
+                pageNum: 5
+            });
+        };
+    });
+    */
+    res.render('users',{
+        title: 'users',
+        user: req.user,
+        pageNum: 5
+    });
 });
+
 
 module.exports = router;
